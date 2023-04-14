08dba52b7aa8a2d8fe5ebbafe1bd72090588a2a1
Deekshith kumar Vobilisetty <61815031+bunny845@users.noreply.github.com>
2023-04-14 11:52:14 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,7 +1,5 @@
 import com.github.music.of.the.ainur.almaren.Almaren
 object Main extends LazyLogging {
-  val almaren = Almaren("App Name")
-  val spark = almaren.spark.master("local[*]").config("spark.sql.shuffle.partitions", "1").getOrCreate()
   def main(args:Array[String]) : Unit={
     val data = spark.range(10)
     data.count();
